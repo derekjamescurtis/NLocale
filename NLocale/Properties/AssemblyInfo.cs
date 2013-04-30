df2bfd9ea7a1924542f66c704918ec0395b73d58
Derek J. Curtis <djcurtis@summersetsoftware.com>
2013-05-01 06:12:08 +0700

    Changed directory structure -- added assembly information to main project.

--- NLocale/Properties/AssemblyInfo.cs
+++ NLocale/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Locale.SubCountry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NLocale")]
+[assembly: AssemblyDescription(".NET port of Perl's Locale::SubCountry")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Locale.SubCountry")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Summerset Software, LLC")]
+[assembly: AssemblyProduct("NLocale")]
+[assembly: AssemblyCopyright("Copyright © Derek J. Curtis 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
